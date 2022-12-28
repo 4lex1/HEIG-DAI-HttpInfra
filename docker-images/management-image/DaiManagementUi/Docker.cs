732f7d8ed38e4849863c97375487dd5db430d55a
alexis-heig <alexis.monthoux@heig-vd.ch>
2022-12-28 13:24:42 +0100

    added a homemade management UI to manage containers

---
+++ docker-images/management-image/DaiManagementUi/Docker.cs
@@ -0,0 +1,81 @@
+﻿using Docker.DotNet.Models;
+using Docker.DotNet;
+
+namespace DaiManagementUi
+{
+    public class Docker
+    {
+        public static List<ContainerListResponse> GetAllContainers()
+        {
+            var client = GetClient();
+            var parameters = new ContainersListParameters
+            {
+                All = true
+            };
+
+            return client.Containers.ListContainersAsync(parameters).Result.ToList();
+        }
+
+        public static void StartContainer(string id)
+        {
+            if (IsRunning(id))
+            {
+                return;
+            }
+
+            var client = GetClient();
+            var parameters = new ContainerStartParameters();
+            client.Containers.StartContainerAsync(id, parameters).Wait();
+        }
+
+        public static void StopContainer(string id)
+        {
+            if (!IsRunning(id))
+            {
+                return;
+            }
+
+            var client = GetClient();
+            var parameters = new ContainerStopParameters();
+            client.Containers.StopContainerAsync(id, parameters).Wait();
+        }
+
+        public static bool IsRunning(string id)
+        {
+            var container = GetAllContainers().FirstOrDefault(c => c.ID == id);
+            if (container != null)
+            {
+                return container.State == "running";
+            }
+
+            return false;
+        }
+
+        public static void CreateContainer(string tag)
+        {
+            var client = GetClient();
+            var parameters = new ImagesCreateParameters
+            {
+                FromImage = tag
+            };
+            client.Images.CreateImageAsync(parameters, null, new Progress<JSONMessage>()).Wait();
+            client.Containers.CreateContainerAsync(new CreateContainerParameters()
+            {
+                Image = tag
+            }).Wait();
+        }
+
+        public static void DeleteContainer(string id)
+        {
+            var client = GetClient();
+            var parameters = new ContainerRemoveParameters 
+            { 
+                Force = true 
+            };
+            client.Containers.RemoveContainerAsync(id, parameters).Wait();
+        }
+
+        private static DockerClient GetClient()
+            => new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock")).CreateClient();
+    }
+}
---
+++ docker-images/management-image/DaiManagementUi/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Docker.DotNet.Models;
+using Docker.DotNet;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace DaiManagementUi.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private IActionResult DoAction(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception ex)
+            {
+                TempData["Message"] = ex.ToString();
+            }
+            return Redirect("/");
+        }
+
+        public IActionResult OnGetStart(string id)
+            => DoAction(() => Docker.StartContainer(id));
+
+        public IActionResult OnGetStop(string id)
+            => DoAction(() => Docker.StopContainer(id));
+
+        public IActionResult OnPost(string tag)
+            => DoAction(() => Docker.CreateContainer(tag));
+
+        public IActionResult OnGetDelete(string id)
+            => DoAction(() => Docker.DeleteContainer(id));
+    }
+}
